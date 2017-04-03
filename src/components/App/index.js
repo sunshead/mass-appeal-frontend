55302fa60a53e4709e67bda91683652bf5c1e6a6
Chris Praktish <cpraktish@intrepid.io>
2017-04-03 13:02:47 -0400

    set up campaign component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,10 +5,12 @@ import HomeContainer from 'containers/HomeContainer';
 import LogInContainer from 'containers/LogInContainer';
 import AuthRoute from 'containers/AuthRoute';
 import Private from 'components/Private';
+import Campaign from 'components/Campaign/Campaign';
 
 const App = () => (
   <Layout>
     <Route exact path="/" component={HomeContainer} />
+    <Route path="/campaign" component={Campaign} />
     <Route path="/login" component={LogInContainer} />
     <AuthRoute path="/private" component={Private} />
   </Layout>
