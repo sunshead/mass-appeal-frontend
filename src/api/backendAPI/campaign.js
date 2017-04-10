bf143cb2b65afde998a0303c5f1f9ffab0b3df0f
Chris Praktish <cpraktish@intrepid.io>
2017-04-10 12:47:31 -0400

    Add endpoints to save and delete pledges

--- src/api/backendAPI/campaign.js
+++ src/api/backendAPI/campaign.js
@@ -111,14 +111,60 @@ class CampaignAPI {
       });
   }
 
-  static deleteCampaign(data) {
-    return fetch(`${MassAppealAPIDomain}/api/campaigns/${data.opportunityId}`, {
+  static savePledge(data) {
+    return fetch(`${MassAppealAPIDomain}/api/pledges/${data.pledgeId}`, {
+      method: 'POST',
+      headers: authorizationHeadersV1(),
+    })
+      .then(response => {
+        let { ok, status, statusText } = response;
+        if (ok) {
+          return response.json();
+        } else {
+          let error = new Error(`${status} (${statusText})`);
+          throw error;
+        }
+      })
+      .then(body => {
+        if (body) {
+          return body;
+        } else {
+          throw new Error('Unable to save pledge');
+        }
+      });
+  }
+
+  static deletePledge(data) {
+    return fetch(`${MassAppealAPIDomain}/api/pledges/${data.pledgeId}`, {
       method: 'DELETE',
       headers: authorizationHeadersV1(),
     })
       .then(response => {
         let { ok, status, statusText } = response;
-        if (ok || status === 422) {
+        if (ok) {
+          return response.json();
+        } else {
+          let error = new Error(`${status} (${statusText})`);
+          throw error;
+        }
+      })
+      .then(body => {
+        if (body) {
+          return body;
+        } else {
+          throw new Error('Unable to delete pledge');
+        }
+      });
+  }
+
+  static deleteCampaign(data) {
+    return fetch(`${MassAppealAPIDomain}/api/campaigns/${data.campaignId}`, {
+      method: 'DELETE',
+      headers: authorizationHeadersV1(),
+    })
+      .then(response => {
+        let { ok, status, statusText } = response;
+        if (ok) {
           return response.json();
         } else {
           let error = new Error(`${status} (${statusText})`);
